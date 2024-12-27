38b0704e736d651fdcf7c5cd73eca057499878ef
Evgenii Chkalov <evgeniic@lucanet.com>
2024-12-27 18:27:15 +0100

    init

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // Take TG bot's token
 const TOKEN = process.env.TELEGRAM_API_TOKEN;
+const MESSAGES_LIMIT = 50;
 
 // Storage for messages
-let messages = {};
-let bot;
+const messages = {};
 
 // Function to get today's date in YYYY-MM-DD format
 const getTodayDate = () => {
@@ -13,13 +13,13 @@ const getTodayDate = () => {
 };
 
 // Initialize bot with polling
-bot = new TelegramBot(TOKEN, { polling: true });
+const bot = new TelegramBot(TOKEN, { polling: true });
 console.log('Bot initialized');
 
 // Handle incoming messages
 bot.on('message', (msg) => {
-    console.log('Message received: ', msg.text);
     try {
+        console.log('Message received', msg);
         const chatId = msg.chat.id;
         const today = getTodayDate();
 
@@ -27,17 +27,23 @@ bot.on('message', (msg) => {
         if (!messages[chatId]) messages[chatId] = {};
         if (!messages[chatId][today]) messages[chatId][today] = [];
 
+        if (msg.text.includes('@LabyrinthNewsletterBot')) {
+            return;
+        }
+
         // Add the message to today's list
         messages[chatId][today].push(msg.text || '[Не-текстовое-сообщение]');
+        if (messages[chatId][today].length >= MESSAGES_LIMIT) {
+            messages[chatId][today].shift();
+        }
     } catch (ex) {
         console.error(ex)
     }
-
 });
 
 // Handle the "/show_newsletter" command
-bot.onText(/\/show_newsletter/, (msg) => {
-    console.log('/show_newsletter command received');
+bot.onText(/@LabyrinthNewsletterBot/, (msg) => {
+    console.log('Newsletter command received');
     try {
         const chatId = msg.chat.id;
         const today = getTodayDate();
@@ -52,12 +58,15 @@ bot.onText(/\/show_newsletter/, (msg) => {
         }
 
         // If no messages are found for today, notify the user
-        if (todayMessages.length >= 50) {
-            bot.sendMessage(chatId, 'Будет проанализировано только последние 50 сообщений (API, хостинг, и AI токены не бесплатные!). Пожертвуйте лысому на пиво и массаж для разблокировки безлимитной версии :)');
+        if (todayMessages.length >= MESSAGES_LIMIT) {
+            bot.sendMessage(chatId, `Будет проанализировано только последние ${MESSAGES_LIMIT} сообщений (API, хостинг, и AI токены не бесплатные!). Пожертвуйте лысому на пиво и массаж для разблокировки безлимитной версии :)`);
         }
 
         // Send all accumulated messages for today
         bot.sendMessage(chatId, `Сообщения за сегодня:\n\n${todayMessages.join('\n')}`);
+
+        // Clean up messages for today
+        messages[chatId][today] = [];
     } catch (ex) {
         console.error(ex)
     }
